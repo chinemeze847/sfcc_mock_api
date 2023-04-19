e795f9109d997ac19033ccac67ec11e011dfcd79
Chinemeze <thankgodchinemeze@gmail.com>
2023-04-19 17:03:31 +0100

    modified sequelize

--- src/instances/sequelize.ts
+++ src/instances/sequelize.ts
@@ -5,11 +5,11 @@ import {Sequelize} from "sequelize-typescript"
 const db = 'sfccdb'
 const username = 'root'
 const password = 'thankgod'
+console.log(path.join(`./${__dirname}`,'../models'))
 
 export const sequelize = new Sequelize(db, username, password, {
-   host: 'localhost',
+   host: '34.243.62.158',
    dialect: 'mysql',
-   port: 3306,
    models: [path.join(`./${__dirname}`,'../models')]
    // models: [__dirname + '../models'] 
 });
